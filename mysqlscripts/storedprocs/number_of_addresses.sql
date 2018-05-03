15e96fc17f2d80b27d01acc387c80bc9bb4ffaef
ruannelloyd <35781921+ruannelloyd@users.noreply.github.com>
2018-05-03 23:44:08 +0200

    cleanup

--- mysqlscripts/storedprocs/number_of_addresses.sql
+++ mysqlscripts/storedprocs/number_of_addresses.sql
@@ -39,34 +39,6 @@ BEGIN
    group by walletaddress;  
 
    select * from tmp_wallet order by walletbalance DESC limit 10;
-
-    declare var_isexist INT;
-     set var_isexist = 0;
-    select ifnull(count(*),0) into var_isexist
-    from transaction_in
-     where transactionid = p_transactionid
-       and trinid = p_trinid;
-
-     select var_isexist;
-
-     if var_isexist = 0 then
-       insert into transaction_in (blocknumber,transactionid,trinid, vout,sequence,coinbase) values (p_blocknumber,p_transactionid,p_trinid, p_vout,p_sequence,p_coinbase) ;
-     else
-       update transaction_in
-          set vout = p_vout,
-              sequence = p_sequence,
-              coinbase = p_coinbase
-        where transactionid = p_transactionid
-         and trinid = p_trinid;
-     end if;
-
-     update transaction_in a,transaction_out b 
-       set a.amount = b.amount,
-           a.walletaddress = b.walletaddress
-     where a.transactionid = p_transactionid
-       and a.trinid = p_trinid
-       and a.trinid = b.transactionid
-       and a.vout = b.n;
 END$$
  
 DELIMITER ;
